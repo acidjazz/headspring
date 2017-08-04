4c7c5570d8a11e3427428d9459dbc7026981b936
kevin olson <acidjazz@gmail.com>
2017-08-04 01:17:37 -0700

    🎉 employee modification progress

--- web/pages/employees/_id.vue
+++ web/pages/employees/_id.vue
@@ -3,87 +3,194 @@
   Top
   section.section
     .container
-      h1.title Employees
-      h2.subtitle Headspring Employee Directory
+      nav.breadcrumb.is-medium(arial-label="breadcrumbs")
+        ul
+          li: router-link(to="/employees") Employees
+          li.is-active(v-if="$route.params.id === 'new'"): a New
+          li.is-active(v-else): a {{ this.employee.firstname }} {{ this.employee.lastname }}
+  .container.is-narrow
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label Name
+      .field-body
+        .field
+          p.control.has-icons-left.has-icons-right
+            input.input(type="text",placeholder="First Name",v-model="employee.firstname",
+            :class="{'is-danger': this.errors.firstname}")
+            span.icon.is-small.is-left
+              i.fa.fa-user
+            span.icon.is-small.is-right(v-if="employee.firstname !== ''")
+              i.fa.fa-check
+          p.help.is-danger {{ this.errors.firstname }}
+        .field
+          p.control.has-icons-left.has-icons-right
+            input.input(type="text",placeholder="Last Name",v-model="employee.lastname",
+            :class="{'is-danger': this.errors.lastname}")
+            span.icon.is-small.is-left
+              i.fa.fa-user
+            span.icon.is-small.is-right(v-if="employee.lastname !== ''")
+              i.fa.fa-check
+          p.help.is-danger {{ this.errors.lastname }}
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label E-mail Address
+      .field-body
+        .field
+          p.control.has-icons-left.has-icons-right
+            input.input(type="email",v-model="employee.email",
+            :class="{'is-danger': this.errors.email}")
+            span.icon.is-small.is-left
+              i.fa.fa-envelope
+            span.icon.is-small.is-right(v-if="employee.email !== ''")
+              i.fa.fa-check
+          p.help.is-danger {{ this.errors.email }}
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label Job Title
+      .field-body
+        .field
+          p.control.has-icons-left.has-icons-right
+            input.input(type="text",placeholder="Sales Manager",v-model="employee.title",
+            :class="{'is-danger': this.errors.title}")
+            span.icon.is-small.is-left
+              i.fa.fa-id-badge
+            span.icon.is-small.is-right(v-if="employee.title !== ''")
+              i.fa.fa-check
+          p.help.is-danger {{ this.errors.title }}
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label Active
+      .field-body
+        .field.is-narrow
+          .control
+            label.checkbox
+              input(type="checkbox",v-model="employee.active")
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label Address
+      .field-body
+        .field
+          p.control.has-icons-left.has-icons-right
+            input.input(type="text",placeholder="Street",v-model="employee.address.street",
+            :class="{'is-danger': this.errors['address.street']}")
+            span.icon.is-small.is-left
+              i.fa.fa-address-card
+            span.icon.is-small.is-right(v-if="employee.address.street !== ''")
+              i.fa.fa-check
+          p.help.is-danger {{ this.errors['address.street'] }}
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label
+      .field-body
+        .field
+          p.control
+            input.input(type="text",placeholder="City",v-model="employee.address.city",
+            :class="{'is-danger': this.errors['address.city']}")
+          p.help.is-danger {{ this.errors['address.city'] }}
+        .field
+          p.control.is-narrow
+            input.input(type="text",placeholder="State",v-model="employee.address.state",
+            :class="{'is-danger': this.errors['address.state']}")
+          p.help.is-danger {{ this.errors['address.state'] }}
+        .field.is-danger
+          p.control
+            input.input(
+            type="text", placeholder="Zip Code", v-model="employee.address.zip",
+            :class="{'is-danger': this.errors['address.zip']}")
+          p.help.is-danger {{ this.errors['address.zip'] }}
 
-      .columns
-        .column
-          .field.is-horizontal
-            .field-label.is-normal
-              label.label Name
-            .field-body
-              .field
-                p.control.has-icons-left
-                  input.input(type="text",placeholder="First Name")
-                  span.icon.is-small.is-left
-                    i.fa.fa-user
-              .field
-                p.control.has-icons-left
-                  input.input(type="text",placeholder="Last Name")
-                  span.icon.is-small.is-left
-                    i.fa.fa-user
-          .field.is-horizontal
-            .field-label.is-normal
-              label.label E-mail Address
-            .field-body
-              .field
-                p.control.has-icons-left
-                  input.input(type="email")
-                  span.icon.is-small.is-left
-                    i.fa.fa-envelope
-          .field.is-horizontal
-            .field-label.is-normal
-              label.label Job Title
-            .field-body
-              .field
-                p.control.has-icons-left
-                  input.input(type="text",placeholder="Sales Manager")
-                  span.icon.is-small.is-left
-                    i.fa.fa-id-badge
-          .field.is-horizontal
-            .field-label.is-normal
-              label.label Active
-            .field-body
-              .field.is-narrow
-                .control
-                  label.checkbox
-                    input(type="checkbox")
-          .field.is-horizontal
-            .field-label.is-normal
-              label.label Address
-            .field-body
-              .field
-                p.control.has-icons-left
-                  input.input(type="text",placeholder="Street")
-                  span.icon.is-small.is-left
-                    i.fa.fa-address-card
-          .field.is-horizontal
-            .field-label.is-normal
-              label.label
-            .field-body
-              .field
-                p.control
-                  input.input(type="text",placeholder="City")
-              .field
-                p.control
-                  input.input(type="text",placeholder="State")
-              .field
-                p.control
-                  input.input(type="text",placeholder="Zip Code")
-          .field.is-horizontal
-            .field-label.is-normal
-              label.label
-            .field-body
-              .field
-                p.control
-                  button.button.is-primary Add Employee
+    .field.is-horizontal
+      .field-label.is-normal
+        label.label
+      .field-body
+        .field
+          p.control
+            button.button.is-primary(v-if="$route.params.id === 'new'",@click="save()") Add Employee
+            button.button.is-primary(v-else,@click="save()") Update Employee
 
+  Toast(ref='toast')
 </template>
 
 
 <script>
 import Top from '~/components/Top'
+import Toast from '~/components/Toast.vue'
+import axios from 'axios'
 export default {
-  components: { Top },
+
+  components: { Top, Toast },
+
+  validate ({ params }) {
+    return /^[0-9a-fA-F]{24}|new/.test(params.id)
+  },
+
+  methods: {
+
+    get () {
+      axios.get('http://localhost:8000/api/employees', {params: {_id: this.$route.params.id}})
+      .then(result => {
+        this.result = result
+        this.employee = result.data.data[0]
+      })
+    },
+
+    save () {
+      axios.post('http://localhost:8000/api/employees/modify', this.employee)
+      .then(result => {
+        this.toast({
+          type: 'success',
+          title: 'Success',
+          body: result.data.data.status
+        })
+
+      })
+      .catch(error => {
+        console.log(error)
+        if (error.response !== undefined) {
+          this.error(error.response.data.errors)
+        }
+      })
+    },
+
+    toast (params) {
+      this.refs.$toast.add(params)
+    },
+
+    error (errors) {
+      this.errors = {}
+      for (let error of errors) {
+        this.errors[error.type] = error.message
+      }
+    },
+
+  },
+
+  mounted () {
+    if (this.$route.params.id !== 'new') {
+      this.get()
+    }
+  },
+
+  data () {
+
+    return {
+      result: undefined,
+      errors: {},
+      employee: {
+        id: false,
+        firstname: '',
+        lastname: '',
+        email: '',
+        title: '',
+        address: {
+          street: '',
+          city: '',
+          state: '',
+          zip: '',
+        },
+        active: true,
+      }
+    }
+  }
 }
 </script>
--- web/pages/employees/index.vue
+++ web/pages/employees/index.vue
@@ -3,76 +3,85 @@
   Top
   section.section
     .container
-      h1.title Employees
-      h2.subtitle Headspring Employee Directory
-      .data.has-text-centered.has-text-primary(v-if="api.result === undefined")
-        span.icon.is-large
-          i.fa.fa-spinner.fa-pulse
+      nav.breadcrumb.is-medium(arial-label="breadcrumbs")
+        ul
+          li: router-link(to="/employees") Employees
+          li.is-active: a List
+  .container
+    .has-text-centered.has-text-primary(v-if="api.result === undefined")
+      span.icon.is-large
+        i.fa.fa-spinner.fa-pulse
 
-      .data.has-text-centered(v-else-if="api.result.data.length === 0")
-        span.icon
-          i.fa.fa-bell
-        span &nbsp; No employees found
-      .data(v-else)
+    .has-text-centered(v-else-if="api.result.data.length === 0")
+      span.icon
+        i.fa.fa-bell
+      span &nbsp; No employees found
+    div(v-else)
 
-        nav.level
-          .level-left
-            .level-item
-              p.subtitle.is-5
-                strong {{ numeral(api.result.paginate.total).format('0,0') }} 
-                | Employees
-            .level-item
-              .field.has-addons
-                p.control
-                  input.input(type="text",placeholder="Find an Employee")
-                p.control
-                  button.button Search
-          .level-right
-            p.level-item(v-if="api.params.active === 'both'")
-              strong All
-            p.level-item(v-else)
-              a(@click="api.params.active = 'both'") All
+      nav.level
+        .level-left
+          .level-item
+            p.subtitle.is-5
+              strong {{ numeral(api.result.paginate.total).format('0,0') }} 
+              | Employees
+          .level-item
+            .field.has-addons
+              p.control
+                input.input(type="text",placeholder="Find an Employee")
+              p.control
+                button.button Search
+        .level-right
+          p.level-item(v-if="api.params.active === 'both'")
+            strong All
+          p.level-item(v-else)
+            a(@click="api.params.active = 'both'") All
 
-            p.level-item(v-if="api.params.active === true")
-              strong Active
-            p.level-item(v-else)
-              a(@click="api.params.active = true") Active
+          p.level-item(v-if="api.params.active === true")
+            strong Active
+          p.level-item(v-else)
+            a(@click="api.params.active = true") Active
 
-            p.level-item(v-if="api.params.active === false")
-              strong Inactive
-            p.level-item(v-else)
-              a(@click="api.params.active = false") Inactive
+          p.level-item(v-if="api.params.active === false")
+            strong Inactive
+          p.level-item(v-else)
+            a(@click="api.params.active = false") Inactive
 
-            p.level-item
-              router-link.button.is-primary(to="/employees/new") New
+          p.level-item
+            router-link.button.is-primary(to="/employees/new") New
 
-        table.table
-          thead
-            tr
-              th Name
-              th E-mail 
-              th Title 
-              th Updated 
-              th Active 
-          transition-group(name="e",tag="tbody")
-            tr(v-for="employee in api.result.data",:key="employee._id").e-item
-              td {{ employee.firstname }} {{ employee.lastname }}
-              td {{ employee.email }}
-              td {{ employee.title }}
-              td
-                Moment(:datetime="employee.updated_at")
-              td(v-if="employee.active").has-text-centered
-                span.icon
-                  i.fa.fa-check.has-text-success
-              td(v-else).has-text-centered
-                span.icon.has-text-danger
-                  i.fa.fa-ban
+      table.table
+        thead
+          tr
+            th Name
+            th E-mail 
+            th Title 
+            th Updated 
+            th Active 
+        transition-group(name="e",tag="tbody")
+          router-link(
+            v-for="employee in api.result.data",
+            :key="employee._id",
+            tag="tr",
+            :to="'/employees/' + employee._id").e-item
+            td {{ employee.firstname }} {{ employee.lastname }}
+            td {{ employee.email }}
+            td {{ employee.title }}
+            td
+              Moment(:datetime="employee.updated_at")
+            td(v-if="employee.active").has-text-centered
+              span.icon
+                i.fa.fa-check.has-text-success
+            td(v-else).has-text-centered
+              span.icon.has-text-danger
+                i.fa.fa-ban
 
-        Paginate(:paginate="api.result.paginate",@change="api.params.page = $event")
+      Paginate(:paginate="api.result.paginate",@change="api.params.page = $event")
   Toast(ref='toast')
 </template>
 
 <style lang="stylus">
+tr
+  cursor pointer
 .e-item 
   transition opacity 1s, transform 1s
 .e-enter, .e-leave-to
